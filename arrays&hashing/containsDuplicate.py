e90e36da42880856c1a15c6018d7acd0a970e47a
Akram BENHAMMOU <aben200244@gmail.com>
2025-09-21 03:04:07 +0200

    containsDuplicate

---
+++ arrays&hashing/containsDuplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        list = set(nums)
+        if len(list) != len(nums) : 
+            return True
+        else :
+            return False
